421013b824beff6d439359f929ef51c955e1c152
vstlvl <zorix228337@gmail.com>
2025-07-29 12:42:41 +0300

    add the main header file to project

---
+++ src/lib/mheader.h
@@ -0,0 +1,22 @@
+/**
+ * @file mheader.h
+ * @author anyvvaay
+ * @brief the main header file
+ * @version 0.1
+ * @date 2025-07-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#pragma once
+
+#include <iostream>
+#include "herror.h"
+
+#define EXT_OF_SRC_FILES ".u"
+#define EXT_OF_HEADER_FILES ".uh"
+#define DEFAULT_OTP_FILE_NAME "a"
+
+#define SUCCESS 0
+#define ERROR 1
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include "./lib/mheader.h"
 
 int
 main(int argc, char** argv){
     if(argc < 2)
-        ;
+        abort_w_msg("Too few arguments", ERROR);
 
-    return 0;
+    
+
+    return SUCCESS;
 }
